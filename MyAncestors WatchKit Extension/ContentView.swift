c00fd1d90fcada7a3e305b96881b79303ecc292f
Kevin Ray <kevraypt@gmail.com>
2019-10-02 11:46:39 -0600

    Initial Commit

---
+++ MyAncestors WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyAncestors WatchKit Extension
+//
+//  Created by Kevin Ray on 10/2/19.
+//  Copyright © 2019 Booyabuddy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyAncestors WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  MyAncestors WatchKit Extension
+//
+//  Created by Kevin Ray on 10/2/19.
+//  Copyright © 2019 Booyabuddy. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
